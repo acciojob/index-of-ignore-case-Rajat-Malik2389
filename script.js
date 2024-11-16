db75cea4237d73aab1063474c10727dc315c22e4
Rajat Malik <133639198+Rajat-Malik2389@users.noreply.github.com>
2024-11-16 17:25:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ return idx
  
  
 // Please do not change the code below
-//const s1 = prompt("Enter s1:");
-//const s2 = prompt("Enter s2:");
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
  
 
